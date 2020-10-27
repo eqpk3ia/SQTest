666b1ae2dcc705b2357f2854281602b787b368cd
Dylan Smith <dylan-smith@github.com>
2020-10-27 03:26:36 -0500

    created project

---
+++ SQTest/SQTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SQTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
